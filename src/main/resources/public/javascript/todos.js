a43001750e1c41414774971b4b41dc738e127dd1
Avery Koranda <koran023@morris.umn.edu>
2019-02-14 22:21:01 -0600

    Comment on js error preventing correct response

--- src/main/resources/public/javascript/todos.js
+++ src/main/resources/public/javascript/todos.js
@@ -4,6 +4,10 @@
 function getAllToDos() {
   console.log("Getting all the todos.");
 
+  /* currently throws an error in the console:
+  * Uncaught ReferenceError: HttpClient is not defined
+    at HTMLButtonElement.getAllToDos (todos.js:7)
+  */
   var HttpThingy = new HttpClient();
   HttpThingy.get("/api/todos", function (returned_json) {
     document.getElementById('jsonDump').innerHTML = returned_json;
