3d3c852f2d8fe73a61b49724006eebd6648ccb8d
Avery Koranda <koran023@morris.umn.edu>
2019-02-18 20:20:33 -0600

    Write tests for contains filter
    143 is a made-up value so tests aren't passing. Need to figure out how many todos ought to be returned.

---
+++ src/test/java/umm3601/todo/FilterToDosByContainsFromDB.java
@@ -0,0 +1,31 @@
+package umm3601.todo;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class FilterToDosByContainsFromDB {
+
+  @Test
+  public void filterToDosByContains() throws IOException {
+    ToDoDatabase db = new ToDoDatabase("src/main/data/todos.json");
+    ToDo[] allToDos = db.listToDos(new HashMap<>());
+
+    ToDo[] containingIncididunt = db.filterToDosByContains(allToDos, "incididunt");
+    assertEquals("Incorrect number of todos returned", 143, containingIncididunt.length);
+  }
+
+  @Test
+  public void listToDosWithContainsFilter() throws IOException {
+    ToDoDatabase db = new ToDoDatabase("src/main/data/todos.json");
+    Map<String, String[]> queryParams = new HashMap<>();
+
+    queryParams.put("contains", new String[]{"incididunt"});
+    ToDo[] containingIncididunt = db.listToDos(queryParams);
+    assertEquals("Incorrect number of todos returned", 143, containingIncididunt.length);
+  }
+}
