794ccf9d563cdffbdeb1c6efa22fb111b538d7df
Avery Koranda <koran023@morris.umn.edu>
2019-02-18 19:42:06 -0600

    Write second test for status filter

--- src/test/java/umm3601/todo/FilterToDosByStatusFromDB.java
+++ src/test/java/umm3601/todo/FilterToDosByStatusFromDB.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -16,12 +17,20 @@ public class FilterToDosByStatusFromDB {
 
     ToDo[] completeToDos = db.filterToDosByStatus(allToDos, "complete");
     ToDo[] incompleteToDos = db.filterToDosByStatus(allToDos, "incomplete");
-    
-    assertEquals("Number of complete and incomplete todos don't add up", allToDos.length, completeToDos.length + incompleteToDos.length);
+
+    assertEquals("Number of complete and incomplete todos don't add up", 300, completeToDos.length + incompleteToDos.length);
   }
 
   @Test
-  public void listToDosWithLimitFilter() {
+  public void listToDosWithStatusFilter() throws IOException {
+    ToDoDatabase db = new ToDoDatabase("src/main/data/todos.json");
+    Map<String, String[]> queryParams = new HashMap<>();
 
+    queryParams.put("status", new String[]{"complete"});
+    ToDo[] completeToDos = db.listToDos(queryParams);
+
+    queryParams.put("status", new String[]{"incomplete"});
+    ToDo[] incompleteToDos = db.listToDos(queryParams);
+    assertEquals("Incorrect number of todos", 300, completeToDos.length + incompleteToDos.length);
   }
 }
